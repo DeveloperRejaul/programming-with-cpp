cb85d2beaeb96b271977246e5aa3e99e82093fd8
Rezaul <mac@Rezauls-MacBook-Air.local>
2026-03-06 00:50:53 +0600

    learn variable

---
+++ 04.variables/app.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // create variable
+    //type variable_name = value;
+    
+    // craete number variable
+    int int_number = 5;
+    cout << "int_number is: " << int_number << "\n";
+
+    // crate decimal variable
+    double double_number = 3.14;
+    cout << "double_number is: " << double_number << "\n";
+
+    // create character variable
+    char character = 'A';
+    cout << "character is: " << character << "\n";
+
+
+    // crate strig variable
+    string str = "Hello, World!";
+    cout << "str is: " << str << "\n";
+
+    // crate boolean variable
+    bool is_true = false;
+    cout << "is_true is: " << is_true << "\n";
+    
+
+    // Declare Multiple Variables
+
+    int a =1 , b=2, c=3;
+    cout << "Sum of a, b, c is: " << a+b+c << "\n";
+
+
+    // constant variable
+    const double PI = 3.14159;
+    cout << "const PI is: " << PI << "\n";
+
+    return 0;
+}
