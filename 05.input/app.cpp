a912c6696d2459cb81c1e0149a91cd304c2d897d
Rezaul <mac@Rezauls-MacBook-Air.local>
2026-03-06 01:02:40 +0600

    learn input

---
+++ 05.input/app.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+    // cout means output
+    // cin means input
+
+
+    // input from user
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Your number is: " << x << "\n";
+
+    return 0;
+}
