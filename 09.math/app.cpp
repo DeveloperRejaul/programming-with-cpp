e5c51fe44715efc8e9f912edd74f91295e652241
Rezaul <mac@Rezauls-MacBook-Air.local>
2026-03-06 22:08:31 +0600

    learn math

---
+++ 09.math/app.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    // **Mathematical all functions**
+    // max 
+    cout << "Max of 10 and 20: " << max(10, 20) << endl; // Output: Max of 10 and 20: 20
+
+    // min
+    cout << "Min of 10 and 20: " << min(10, 20) << endl; // Output: Min of 10 and 20: 10
+
+    // C++ <cmath> Library
+    cout << sqrt(64) << endl; // Output: 8
+    cout << pow(2, 3) << endl; // Output: 8
+    cout << round(2.6) << endl; // Output: 3
+    cout << log(2) << endl; // Output: 0.693147
+
+    return 0;
+}
